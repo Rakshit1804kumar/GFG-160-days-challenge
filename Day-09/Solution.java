d63bd8fef7bd96c51f580b4cb93a743d9d6a3191
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-02-06 14:48:05 +0530

    Create Solution.java

---
+++ Day-09/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getMinDiff(int[] arr, int k) {
+ 
+        int n = arr.length;
+        Arrays.sort(arr);
+        int res = arr[n-1] - arr[0];
+         
+        for(int i=1; i<n; i++){
+            if(arr[i]-k < 0){
+                continue;
+            }
+            int minD =Math.min(arr[0]+k , arr[i]-k );
+            int maxD = Math.max(arr[i-1]+k, arr[n-1] - k);
+            res = Math.min(res,maxD-minD);
+        }
+        return res;
+    }
+}
