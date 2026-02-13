ff3d07c4169db1b4a0b1cf99a424a45d2ee7eb97
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-02-13 18:58:27 +0530

    Create Solution 2.java

---
+++ Day-11/Solution 2.java
@@ -0,0 +1,26 @@
+class Solution {
+    int maxProduct(int[] arr) { 
+        int n = arr.length;
+        int max = arr[0];
+        
+        int prefix = 1;
+        int suffix = 1;
+        
+        for(int i=0; i<n; i++){
+            
+            if(prefix == 0){
+                prefix= 1;
+            }
+            
+            if(suffix == 0){
+                suffix = 1;
+            }
+            
+            prefix = prefix * arr[i];
+            suffix = suffix * arr[n-i-1];
+            
+            max =  Math.max(max, Math.max(prefix, suffix));
+        }
+        return max;
+    }
+}
