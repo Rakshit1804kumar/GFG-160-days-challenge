78edda177b9c3954935800067daaa935458cbec2
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-02-17 12:15:37 +0530

    Create Solution 1.java

---
+++ Day-17/Solution 1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+     
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            boolean found = false;
+            
+            for(int j = 0; j<n; j++){
+                if(i != j && s.charAt(i) == s.charAt(j)){
+                    found  = true;
+                    break;
+                }
+            }
+            if(!found) return s.charAt(i);
+        }
+        return '$';
+    }
+}
