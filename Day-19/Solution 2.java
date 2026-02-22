b966350f1e1a2512bf60ce272ee6d8faef0dc0ab
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-02-22 22:26:52 +0530

    Implement minChar method to find minimum characters

--- Day-19/Solution 2.java
+++ Day-19/Solution 2.java
@@ -1,1 +1,32 @@
 
+class Solution {
+    public int minChar(String s) {
+   
+        String rev = new StringBuilder(s).reverse().toString();
+        String combine = s+"*"+rev;
+        
+        int n = combine.length();
+        int[] lps = new int[n];
+        
+        int len = 0;
+        int i=1;
+        
+        while(i<n){
+            if(combine.charAt(i) == combine.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if(len!=0){
+                    len = lps[len-1];
+                }else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        return s.length()- lps[n-1];
+    }
+     
+}
